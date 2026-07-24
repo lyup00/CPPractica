baef3eeb5e12f2143c384aef32ccc5a25e0ffc71
lyup00 <mediina.moriis@gmail.com>
2026-07-24 19:23:05 +0000

    Update8

--- dia5/HashEmbeddingCollisions.cpp
+++ dia5/HashEmbeddingCollisions.cpp
@@ -4,28 +4,23 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    int n, m, id, bucket, collidin = 0;
+    ll n, m, id, bucket, collidin = 0LL;
     cin>>n>>m;
 
-    multiset<int> miset;
+    multiset<ll> miset;
 
-    for(int a=0; a<n; ++a){
+    for(ll a=0; a<n; ++a){
         cin>>id;
         bucket = id % m;
         miset.insert(bucket);
     }
 
-    set <int> veces;
-    for(int e=*miset.begin(); e<=*miset.rbegin(); ++e){
-        veces.insert(e);
-    };
-
-    for(int i=*veces.begin(); i<=*veces.rbegin(); ++i){
+    set <ll> veces(miset.begin(), miset.end());
+    for(ll i: veces){
         if(miset.count(i) != 1){
             collidin += miset.count(i);
         }
     }
-
     cout<<collidin<<endl;
     return 0;
 }
